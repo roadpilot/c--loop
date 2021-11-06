c455f2d380018a922281cc99b8a7f9d5f938688c
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-11-06 12:19:24 +0000

    test loop

--- main.cpp
+++ main.cpp
@@ -3,3 +3,15 @@
 //If 1>=n>=9, then print the English representation of it in lowercase. That is "one" for 1, "two" for 2, and so on.
 //Else if  and it is an even number, then print "even".
 //Else if  and it is an odd number, then print "odd".
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    for(int i = 0; i < 10; i++) {
+        cout<<i<<endl;
+    }
+    return 0;
+}
