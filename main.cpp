c35c8eb5ca11c4a6fe3504fe46de8a5cb0b7e01d
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-11-06 12:33:59 +0000

    conditional even

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-//For each integer  in the inclusive interval :
+//For each integer n in the inclusive interval a-b:
 
-//If 1>=n>=9, then print the English representation of it in lowercase. That is "one" for 1, "two" for 2, and so on.
+//If 1<=n<=9, then print the English representation of it in lowercase. That is "one" for 1, "two" for 2, and so on.
 //Else if  and it is an even number, then print "even".
 //Else if  and it is an odd number, then print "odd".
 
@@ -18,6 +18,9 @@ int main() {
     for(int i = a; i <= b; i++) {
         if(i<=9){
             cout<<intMap[i]<<endl;
+        }
+        else if (i%2==0){
+            cout<<"even"<<endl;
         }
     }
     return 0;
