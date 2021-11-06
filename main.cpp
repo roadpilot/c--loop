48fc63d5db6683a7e603d3b9e29a13b2802a352f
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-11-06 12:18:32 +0000

    problem

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
+//For each integer  in the inclusive interval :
 
-int main() {
-  std::cout << "Hello World!\n";
-} 
+//If 1>=n>=9, then print the English representation of it in lowercase. That is "one" for 1, "two" for 2, and so on.
+//Else if  and it is an even number, then print "even".
+//Else if  and it is an odd number, then print "odd".
